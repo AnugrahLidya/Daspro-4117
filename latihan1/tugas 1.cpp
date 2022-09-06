f4fa7ee19cd8a9ea587a9e1377a2e174710d0255
AnugrahLidya <112890340+AnugrahLidya@users.noreply.github.com>
2022-09-06 07:32:23 +0700

    Add files via upload

---
+++ latihan1/tugas 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama         : Anugrah Lidya Fatmawati" << endl;
+    cout << "Nim          : A11.2022.14653 " << endl;
+    cout << "Asal Sekolah : SMAN 1 REMBANG, JAWA TENGAH " << endl;
+    return 0;
+}
