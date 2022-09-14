8abd9aa79080cc1ab989c04ffa6949729af81295
AnugrahLidya <112890340+AnugrahLidya@users.noreply.github.com>
2022-09-14 11:49:04 +0700

    Add files via upload

---
+++ pertemuan 3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    //integer
+    int x = 3000;
+    cout << "Hello world!" << x;
+    /*
+    */
+    int z;
+    z = 10;
+
+    int u = 18;
+    int y = 40;
+    float t = 162.5;
+    char b = 'L';
+    string ub = "XL";
+
+    cout << "Umur Saya: " << u;
+    cout << "\nUkuran Sepatu: " << y;
+    cout << "\nTinggi Saya: " << t;
+    cout << "\nUkuran Baju: " << b;
+
+    return 0;
+}
