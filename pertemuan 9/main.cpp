bd4fc6d582c78447f902972827dd6cd9bdf7dc1d
AnugrahLidya <112890340+AnugrahLidya@users.noreply.github.com>
2022-10-07 22:02:46 +0700

    Add files via upload

---
+++ pertemuan 9/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/*int main ()
+{
+
+int x;
+ do{
+    if (x % 5 == 0){
+    cout << x << endl;
+    }
+    x++;
+   }while (x<10);
+}*/
+
+int main()
+{
+int x;
+ for (x=0; x<=40; x+=5){
+      cout << x << endl;
+ }
+
+    return 0;
+
+}
