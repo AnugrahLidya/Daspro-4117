d20dfd756e70205c69f83fb629e7372b15f56c9a
AnugrahLidya <112890340+AnugrahLidya@users.noreply.github.com>
2022-11-23 11:40:19 +0700

    Add files via upload

---
+++ loop3.c/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //soal 3
+    int i;
+    for (int i = 100; i > 0; i--){
+        if (i == 54){
+            break;
+        }
+        cout << i << endl;
+    }
+
+    return 0;
+}
