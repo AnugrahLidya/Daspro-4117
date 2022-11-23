ffe2186318668ffaa4a8f390d92ecac604e761cc
AnugrahLidya <112890340+AnugrahLidya@users.noreply.github.com>
2022-11-23 11:38:16 +0700

    Add files via upload

---
+++ loop1.c/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //soal 1
+    int x,y;
+    for (x = 100, y = 1; x > 0; x--, y++){
+        cout << x << " " << y << endl;
+        }
+
+    //soal 2
+
+    return 0;
+}
